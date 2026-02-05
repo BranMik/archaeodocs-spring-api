fd72bd3c81ad0bb0f4444000d5d76e0a69f9ef42
BranMik <bmikusic@gmail.com>
2026-02-05 23:05:37 +0100

    Novi bug resolve u Find

--- src/main/java/com/branko/portfolio/archaeodocs/domain/Find.java
+++ src/main/java/com/branko/portfolio/archaeodocs/domain/Find.java
@@ -25,14 +25,14 @@ public class Find {
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "material_fk", insertable = false, updatable = false)
+    @JoinColumn(name = "material_fk")
     private Material material;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "period_fk", insertable = false, updatable = false)
+    @JoinColumn(name = "period_fk")
     private Period period;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "site_fk", insertable = false, updatable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "site_fk", nullable = false)
     private Site site;
 }
